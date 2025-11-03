2f98eee11b0750a917745fe1768e2962470e71a8
Austin Duncan <your_email@example.com>
2025-11-03 17:42:58 -0600

    vault secured

---
+++ index.js
@@ -0,0 +1,18 @@
+let code1;
+
+code1= 7+3;
+
+let code2;
+code2= (100-60) / 1;
+
+let code3;
+code3= 99 % 60
+
+let message = "the vault has been secured. The combination is:";
+
+let codeA;
+codeA= code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message, codeA, codeB);
